1dd7f0899fa23f4d786e8cf6eb110a1594811039
Alexandre Fagundes <afagundes@kpmg.com.br>
2023-07-21 09:36:07 -0300

    Teste com projeto core

---
+++ projetoAspNetCoreWebbAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetoAspNetCoreWebbAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
